568ed08064141c5e0ea6714c2cb4cb95672286bc
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-05 16:05:05 -0500

    Remove runner from die.py

--- die.py
+++ die.py
@@ -3,8 +3,5 @@ class Die:
     def __init__(self, side_count):
         self.side_count = side_count
     def roll(self):
-        random.randint(0, self.side_count)
+        return random.randint(0, self.side_count)
 
-die = Die(6)
-result = die.roll()
-print(result)
