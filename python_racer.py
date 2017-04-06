50aedbfcb84fbd5939254013bd9fd11b91894259
Mike Bonetti <mbonettimedia@gmail.com>
2017-04-06 10:13:37 -0500

    Add board_viz and adv_player methods

--- python_racer.py
+++ python_racer.py
@@ -1,9 +1,10 @@
 class PythonRacer:
-    def __init__(self, die, length = 30):
+    def __init__(self, players, die, length = 30):
         self.die = die
         self.length = length
-        self.player1_postition = 0
-        self.player2_postition = 0
+        self.players = players
+        self.player1_position = 0
+        self.player2_position = 0
     def finished(self):
         self
 
@@ -11,9 +12,14 @@ class PythonRacer:
         self
 
     def advance_players(self):
-        self
+        self.player1_position += self.die.roll()
+        self.player2_position += self.die.roll()
 
     def board_visualization(self):
-        self
-        
+        player1_track = [' '] * self.length
+        player2_track = [' '] * self.length
+        player1_track[self.player1_position] = self.players[0]
+        player2_track[self.player2_position] = self.players[1]
+        print('| '.join(player1_track))
+        print('| '.join(player2_track))
     
--- runner.py
+++ runner.py
@@ -2,5 +2,10 @@ from die import Die
 from python_racer import PythonRacer
 
 die = Die(6)
-game = PythonRacer(die)
-print(game.die.roll())
+players = ['a', 'b']
+game = PythonRacer(players, die)
+
+while True:
+
+  game.board_visualization()
+  game.advance_players()
